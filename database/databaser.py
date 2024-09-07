3812bd6e8b9b2891863b109a7d7bb81c331352c9
Slava <vskhludnev@gmail.com>
2024-09-07 12:31:13 +0300

    Made function for databaser and simple tests

--- database/databaser.py
+++ database/databaser.py
@@ -6,23 +6,34 @@ class Databaser():
         logger.debug('Initial Databaser class')
         self.connect = sqlite3.connect(database_name)
         self.cursor = self.connect.cursor()
+        self.cursor.execute('CREATE TABLE IF NOT EXISTS Notes(id INTEGER PRIMARY KEY AUTOINCREMENT, title TEXT, login TEXT, password TEXT, other TEXT)')
+        
 
-        try:
-            self.cursor.execute('CREATE TABLE Notes(id integer, title text, login text, password text, other text)')
-        except sqlite3.OperationalError:
-            logger.warning('Table Notes already exists')
-        else:
-            logger.info(f'{database_name} is create')
-
-    def __close(self):
+    def close(self):
         self.connect.commit()
         self.connect.close()
 
-    def add_data(self):
-        pass
+    # Добавление данных в таблицу
+    def add_data(self, data):
+        logger.debug('Initial add_data function')
+        logger.trace(f'passed data: {data}')
+        self.cursor.execute('INSERT INTO Notes (title, login, password, other) VALUES(?, ?, ?, ?)', data)
 
-    def delete_data(self):
-        pass
 
-    def get_data(self):
-        pass
+    # Удаление данных из таблицы
+    def delete_data(self, id):
+        logger.debug(f'Initial delete_data by id={id}')
+        self.cursor.execute('DELETE FROM Notes WHERE id=?', (id,))
+        
+
+    # Вывод общего списка данных (только id и названия)
+    def get_list_data(self):
+        logger.debug('Initial output general information')
+        self.cursor.execute('SELECT id, title FROM Notes')
+        return self.cursor.fetchall()
+
+    # Вывод конкретных данных по id
+    def get_definite_data(self, id):
+        logger.debug(f'Initial output data by id={id}')
+        self.cursor.execute('SELECT * FROM Notes WHERE id=?', (id,))
+        return self.cursor.fetchall()
