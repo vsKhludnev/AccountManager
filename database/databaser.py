b3c92548922f6209541dbdd9702580d1c07dfa40
Slava <vskhludnev@gmail.com>
2024-09-15 09:50:28 +0300

    Rename tables in database and add unit_tests

--- database/databaser.py
+++ database/databaser.py
@@ -6,7 +6,8 @@ class Databaser():
         logger.debug('Initial Databaser class')
         self.connect = sqlite3.connect(database_name)
         self.cursor = self.connect.cursor()
-        self.cursor.execute('CREATE TABLE IF NOT EXISTS Notes(id INTEGER PRIMARY KEY AUTOINCREMENT, title TEXT, login TEXT, password TEXT, other TEXT)')
+        self.cursor.execute('CREATE TABLE IF NOT EXISTS Users (login TEXT, password TEXT)')
+        self.cursor.execute('CREATE TABLE IF NOT EXISTS Accounts (id INTEGER PRIMARY KEY AUTOINCREMENT, title TEXT, login TEXT, password TEXT, other TEXT)')
         
 
     def close(self):
@@ -18,24 +19,24 @@ class Databaser():
     def add_data(self, data):
         logger.debug('Initial add_data function')
         logger.trace(f'passed data: {data}')
-        self.cursor.execute('INSERT INTO Notes (title, login, password, other) VALUES(?, ?, ?, ?)', data)
+        self.cursor.execute('INSERT INTO Accounts (title, login, password, other) VALUES(?, ?, ?, ?)', data)
 
 
     # Удаление данных из таблицы
     def delete_data(self, id):
         logger.debug(f'Initial delete_data by id={id}')
-        self.cursor.execute('DELETE FROM Notes WHERE id=?', (id,))
+        self.cursor.execute('DELETE FROM Accounts WHERE id=?', (id,))
         
 
     # Вывод общего списка данных (только id и названия)
     def get_general_data(self):
-        logger.debug('Initial output general information')
-        self.cursor.execute('SELECT id, title FROM Notes')
+        logger.debug('Initial output general information from Accounts')
+        self.cursor.execute('SELECT id, title FROM Accounts')
         return self.cursor.fetchall()
 
 
     # Вывод конкретных данных по id
     def get_detail_data(self, id):
         logger.debug(f'Initial output data by id={id}')
-        self.cursor.execute('SELECT * FROM Notes WHERE id=?', (id,))
+        self.cursor.execute('SELECT * FROM Accounts WHERE id=?', (id,))
         return self.cursor.fetchall()
--- database/databaser_test.py
+++ database/databaser_test.py
@@ -1,3 +1,11 @@
+def unit_test(test_name, expect, actual):
+    if expect == actual:
+        print(f'\033[32m{test_name}: PASS\033[0m')
+    else:
+        print(f'\033[31m{test_name}: FAIL\033[0m')
+        print(f'expect: {expect}')
+        print(f'actual: {actual}')
+
 from database.databaser import Databaser
 
 if __name__ == '__main__':
@@ -5,15 +13,22 @@ if __name__ == '__main__':
 
     test_data_1 = ('Vk', 'login', 'password', '...')
     test_data_2 = ('Telegramm', 'this login', 'security password', 'other info')
-    
-    print(db.get_general_data())
-    db.add_data(test_data_1)
-    db.add_data(test_data_2)
-    print(db.get_general_data())
-    print(db.get_detail_data(1))
-    print(db.get_detail_data(2))
-    db.delete_data(5)
-    db.delete_data(2)
-    print(db.get_general_data())
 
-    db.close()
+    # Проверка вывода из пустой БД
+    received_data = db.get_general_data()
+    unit_test('Receive general data from emty database', received_data, [])
+
+    receive_detail_data = db.get_detail_data(1)
+    unit_test('Receive detail data by id from empty database', receive_detail_data, [])
+
+
+    # Проверка добавления данных
+
+
+    # Проверка вывода данных после добавления
+
+
+    # Проверка удаления данных
+
+
+    # Проверка вывода данных после удаления
